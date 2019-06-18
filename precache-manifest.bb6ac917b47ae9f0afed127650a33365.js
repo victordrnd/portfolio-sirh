59398ef7f75aee2b7410ec08907684a5b833d1df
Victor Durand <v.durand@piman-consultants.fr>
2019-06-18 16:11:40 +0200

    Updates

---
+++ precache-manifest.bb6ac917b47ae9f0afed127650a33365.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec7da47e2ebce5fc3aa319d9707a3d9",
+    "url": "/portfolio-sirh/index.html"
+  },
+  {
+    "revision": "902141ebb04399a292db",
+    "url": "/portfolio-sirh/static/css/2.59ca6ef4.chunk.css"
+  },
+  {
+    "revision": "902141ebb04399a292db",
+    "url": "/portfolio-sirh/static/js/2.579057f4.chunk.js"
+  },
+  {
+    "revision": "2764c3b1c5cc4f95a94e",
+    "url": "/portfolio-sirh/static/js/main.49e605d2.chunk.js"
+  },
+  {
+    "revision": "4f98f8ccdefd002fedee",
+    "url": "/portfolio-sirh/static/js/runtime~main.f0caaf13.js"
+  }
+]);
