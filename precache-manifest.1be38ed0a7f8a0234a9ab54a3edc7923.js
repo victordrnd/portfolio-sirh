8eb469ee4f54d9df611e736380203c0005604c82
Victor Durand <contact@victordurand.fr>
2019-09-05 17:38:56 +0200

    Updates

---
+++ precache-manifest.1be38ed0a7f8a0234a9ab54a3edc7923.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7083cfc2295d794ca91826c15f403fe6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab20bac8d288d2f0d581",
+    "url": "/static/css/2.848db5fd.chunk.css"
+  },
+  {
+    "revision": "ab20bac8d288d2f0d581",
+    "url": "/static/js/2.4f464c8b.chunk.js"
+  },
+  {
+    "revision": "efa258fdf56e105e539a",
+    "url": "/static/js/main.974c4b0a.chunk.js"
+  },
+  {
+    "revision": "2d1fa8cc8317e0cddb2a",
+    "url": "/static/js/runtime~main.22df95a4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-sirh/precache-manifest.9f951400155ddbe2a396f5fbdd7827b4.js"
+  "/precache-manifest.1be38ed0a7f8a0234a9ab54a3edc7923.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-sirh/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
